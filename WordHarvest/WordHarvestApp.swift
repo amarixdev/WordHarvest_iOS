a8b0ac7ddc4064120b4deaef65a039431c2c5de7
amarixdev <amaridev@alumni.unc.edu>
2024-01-22 20:45:10 -0500

    Initial Commit

---
+++ WordHarvest/WordHarvestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordHarvestApp.swift
+//  WordHarvest
+//
+//  Created by Amari DeVaughn on 1/22/24.
+//
+
+import SwiftUI
+
+@main
+struct WordHarvestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
